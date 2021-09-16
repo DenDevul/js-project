1751ba927bc363e34003432c2def2e955b3ac942
Densup <ggden22@gmail.com>
2021-09-16 22:31:45 +0300

    git init

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/api', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  // console.log(`Example app listening at http://localhost:${port}`)   
+})
