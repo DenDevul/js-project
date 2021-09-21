202d39755ee35f68c6d22c1dd9f99f7d0df523e9
Densup <ggden22@gmail.com>
2021-09-21 18:25:35 +0300

    update task component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,201 +1,29 @@
 <template>
-  <nav class="navbar is-spaced">
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button">
-            Log in
-          </a>
-        </div>
-      </div>
-    </div>
-  </nav>
-  <div class="cards">
-    <div class="block">
-      <a>
-        <span class="icon is-large" style="color: var(--primary-color);">
-          <i class="far fa-3x fa-plus-square"></i>
-        </span>
-      </a>
-    </div>
-    <task
-      :taskId="1"
-      :taskName="'Lorem ipsum dolor sit amet.'"
-      :taskComplete="false"
-    ></task>
-    <task
-      :taskId="2"
-      :taskName="'Lorem ipsum dolor sit amet.'"
-      :taskComplete="false"
-    ></task>
-    <task
-      :taskId="3"
-      :taskName="'Lorem ipsum dolor sit amet.'"
-      :taskComplete="false"
-    ></task>
-    <task
-      :taskId="42"
-      :taskName="'Lorem ipsum dolor sit amet.'"
-      :taskComplete="true"
-    ></task>
-    <div class="my-card">
-      <button>
-        <span>
-          <i class="far fa-square fa-3x"></i>
-        </span>
-      </button>
-      <div class="task">
-        <textarea readonly type="text" ref="edit" id="edit" >Lorem ipsum dolor sit amet.</textarea>
-      </div>
-      <div class="controls">
-        <button v-if="!isEditing" @click="editTask">
-          <span>
-            <i class="far fa-edit fa-3x"></i>
-          </span>
-        </button>
-        <button v-else @click="stopEdit">
-          <span>
-            <i class="far fa-edit fa-3x"></i>
-          </span>
-        </button>
-        <button>
-          <span>
-            <i class="fas fa-times fa-3x"></i>
-          </span>
-        </button>
-      </div>
-    </div>
-  </div>
+  <task taskText="Lorem ipsum dolor sit amet." />
 </template>
 
 <script>
 import Task from "./components/Task.vue";
-// const axios = require("axios");
 
 export default {
   name: "App",
   data() {
     return {
-      isEditing: false,
-      edit: undefined
     };
   },
   components: { Task },
   methods: {
-    // async fetchApi() {
-    //   await axios.get("http://localhost:3000/api").then(response => {
-    //     this.data = response.data;
-    //   });
-    // },
-    editTask() {
-      this.edit.readOnly = false;
-      this.edit.focus()
-      this.isEditing = true;
-    },
-    stopEdit() {
-      this.edit.readOnly = true;
-      this.isEditing = false;
-    },
-    onInput() {
-      this.edit.style.height = "auto";
-      this.edit.style.height = this.edit.scrollHeight + "px";
-    }
   },
   mounted() {
-    this.edit = this.$refs.edit
-    this.edit.addEventListener('input', this.onInput, false);
-    this.edit.addEventListener('click', this.editTask, false)
-    this.edit.addEventListener('blur', this.stopEdit, false)
   }
 };
 </script>
 
 <style>
-.my-card {
-  display: flex;
-  align-items: start;
-  justify-content: end;
-  min-height: 72px;
-  width: 80vw;
-  background-color: var(--primary-color);
-  border-radius: 0.25rem;
-  box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1),
-    0 0 0 1px rgba(10, 10, 10, 0.02);
-  font-size: xx-large;
-  font-weight: bold;
-  color: var(--background-color);
-  padding-top: 10px;
-  padding-left: 5px;
-  padding-right: 5px;
-  margin-bottom: 1.5rem;
-}
-
-.task {
-  display: flex;
-  flex-grow: 1;
-  padding: 0 16px;
-}
-.task textarea {
-  flex-grow: 1;
-  font-weight: inherit;
-  color: inherit;
-  font-size: inherit;
-  background-color: inherit;
-  resize: none;
-  border: none;
-  outline: none;
-  overflow: hidden;
-  cursor: default;
-}
-.controls {
-  min-width: 104px;
-  justify-content: space-between;
-  display: flex;
-}
-
-.my-card button {
-  border: none;
-  background-color: inherit;
-}
-
-button span {
-  color: var(--background-color);
-}
-
-:root {
-  --primary-color: #eeb902;
-  --background-color: #262626;
-  --background-darker: #191919;
-}
-
+@import './assets/style.css';
 body {
   min-width: 100vw;
   min-height: 100vh;
   background-color: var(--background-color);
-}
-.navbar {
-  background-color: var(--background-color);
-}
-.cards {
-  width: 80vw;
-  display: flex;
-  flex-direction: column;
-  margin: 0 auto;
-}
-
-body::-webkit-scrollbar {
-  width: 7px;
-}
-
-body::-webkit-scrollbar-track {
-  background: var(--background-color);
-}
-
-body::-webkit-scrollbar-thumb {
-  background-color: var(--background-darker);
-  border-radius: 5px;
 }
 </style>
--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -1,72 +1,86 @@
 <template>
-  <div class="block card is-size-3">
-    <header class="card-header">
-      <input
-        v-if="isEditing"
-        id="input"
-        class="card-header-title has-text-weight-bold"
-        type="text"
-        ref="edit"
-      />
-      <p v-else class="card-header-title has-text-weight-bold" ref="taskBody">
-        {{ taskId }}. {{ taskName }}
-      </p>
-      <button class="card-header-icon" v-on:click="checkTask">
-        <span class="icon is-medium">
-          <i class="far fa-square" ref="check"></i>
+  <div class="task" ref="task">
+    <button class="check-btn" @click="checkTask">
+      <span>
+        <i class="far fa-square" ref="checkIcon"></i>
+      </span>
+    </button>
+    <div class="task-text">
+      <textarea readonly type="text" ref="text" v-model="this.text" />
+    </div>
+    <div class="task-controls">
+      <button v-if="!isEditing" @click="editTask">
+        <span>
+          <i class="far fa-edit"></i>
         </span>
       </button>
-      <button class="card-header-icon" v-on:click="editTask">
-        <span class="icon is-medium">
-          <i class="fas fa-edit"></i>
+      <button v-else @click="stopEdit">
+        <span>
+          <i class="fas fa-arrow-circle-left"></i>
         </span>
       </button>
-      <button class="card-header-icon">
-        <span class="icon is-medium">
+      <button @click="removeTask">
+        <span>
           <i class="fas fa-times"></i>
         </span>
       </button>
-    </header>
+    </div>
   </div>
 </template>
 
 <script>
-import { nextTick } from "@vue/runtime-core";
 export default {
   name: "Task",
   props: {
     taskId: Number,
-    taskName: String,
+    taskText: String,
     taskComplete: Boolean
   },
   methods: {
-    checkTask() {
-      this.isComplete = !this.isComplete;
-      this.$refs.taskBody.classList.toggle("crossed-line");
-      this.$refs.check.classList.toggle("fa-square");
-      this.$refs.check.classList.toggle("fa-check-square");
-    },
-    async editTask() {
+    editTask() {
+      if (this.isComplete) {
+        this.checkTask();
+      }
+
+      this.$refs.text.readOnly = false;
+      this.$refs.text.focus();
       this.isEditing = true;
-      const temp = this.$refs.taskBody.innerHTML;
-      await nextTick();
-      // console.log(document.getElementById('input'));
-      this.$refs.edit.value = temp;
-      // this.$refs.edit.value = temp
-      // this.$refs.edit.value = this.$refs.taskBody.innerHTML
+    },
+    stopEdit() {
+      this.$refs.text.value = this.$refs.text.value.trim();
+      this.onInput();
+      this.$refs.text.readOnly = true;
+      this.isEditing = false;
+    },
+    onInput() {
+      this.$refs.text.style.height = "auto";
+      this.$refs.text.style.height = this.$refs.text.scrollHeight + "px";
+    },
+    checkTask() {
+      this.$refs.checkIcon.classList.toggle("fa-square");
+      this.$refs.checkIcon.classList.toggle("fa-check-square");
+      this.$refs.text.classList.toggle("crossed-line");
+      this.isComplete = !this.isComplete;
+    },
+    removeTask() {
+      this.$refs.task.remove();
     }
   },
   mounted() {
     if (this.isComplete) {
-      this.$refs.taskBody.classList.toggle("crossed-line");
-      this.$refs.check.classList.toggle("fa-square");
-      this.$refs.check.classList.toggle("fa-check-square");
+      this.$refs.checkIcon.classList.toggle("fa-square");
+      this.$refs.checkIcon.classList.toggle("fa-check-square");
+      this.$refs.text.classList.toggle("crossed-line");
     }
+
+    this.$refs.text.addEventListener("input", this.onInput, false);
+    this.$refs.text.addEventListener("click", this.editTask, false);
+    this.$refs.text.addEventListener("blur", this.stopEdit, false);
   },
   data() {
     return {
       id: this.taskId,
-      name: this.taskName,
+      text: this.taskText,
       isComplete: this.taskComplete,
       isEditing: false
     };
@@ -75,18 +89,96 @@ export default {
 </script>
 
 <style>
-.card {
+.task {
+  width: 80vw;
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template: "check text controls";
+  grid-template-columns: min-content auto min-content;
+  color: var(--background-color);
   background-color: var(--primary-color);
+  border-radius: 0.25rem;
+  box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1),
+    0 0 0 1px rgba(10, 10, 10, 0.02);
+  font-size: xx-large;
+  font-weight: bold;
+  padding: 1rem;
+  margin-bottom: 1.5rem;
 }
+
+.task-text {
+  grid-area: text;
+  display: flex;
+  padding: 0 1rem;
+}
+.task-text textarea {
+  flex-grow: 1;
+  font-weight: inherit;
+  color: inherit;
+  font-size: inherit;
+  background-color: inherit;
+  resize: none;
+  border: none;
+  outline: none;
+  overflow: hidden;
+  cursor: default;
+}
+.task-controls {
+  grid-area: controls;
+  min-width: 104px;
+  margin-bottom: auto;
+  display: flex;
+  justify-content: space-between;
+}
+.check-btn {
+  grid-area: check;
+  margin-bottom: auto;
+}
+.task button {
+  font-size: 2rem;
+  border: none;
+  background-color: inherit;
+}
+.task span {
+  color: var(--background-color);
+}
+
 .crossed-line {
   text-decoration: line-through;
 }
-input {
-  background-color: var(--primary-color);
-  border-style: hidden;
-  outline: none;
-  font-size: inherit;
-  border-radius: inherit;
-  height: 72px;
+
+@media screen and (max-width: 480px) {
+  .task {
+    width: 90vw;
+    margin-left: auto;
+    margin-right: auto;
+    grid-template-areas:
+      "check controls"
+      "text text";
+    grid-template-rows: min-content 1fr;
+    grid-template-columns: 1fr 1fr;
+    padding: 0.5rem;
+    font-size: large;
+  }
+  .task-text {
+    padding: 0;
+  }
+  .task button {
+    font-size: 1.75rem;
+  }
+  .check-btn {
+    margin-right: auto;
+  }
+  .fa-edit,
+  .fa-arrow-circle-left {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .task-controls {
+    min-width: 76px;
+  }
 }
 </style>
